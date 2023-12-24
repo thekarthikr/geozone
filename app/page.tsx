f2c0d898617385b86b5dbb4580efda9226d46b3f
Karthik <karthikwebdev@outlook.com>
2023-12-24 11:48:41 +0530

    add pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
-import Image from 'next/image'
-import { Navbar } from './components/Nav/Navbar'
-import Link from 'next/link'
+import { Metadata } from "next";
+import { SliceZone } from "@prismicio/react";
 
-export default function Home() {
-  return (
-        <>
-          <Navbar/>
-        </>
-  )
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
