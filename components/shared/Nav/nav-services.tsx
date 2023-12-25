e68323fce8fd0d4a84b24c9e28b433201ec1c7a9
Karthik <karthikwebdev@outlook.com>
2023-12-25 13:11:37 +0530

    update navigation

--- components/shared/Nav/nav-services.tsx
+++ components/shared/Nav/nav-services.tsx
@@ -7,35 +7,35 @@ import { NavigationDocumentDataServicesItem, Simplify } from '@/prismicio-types'
 import { GroupField } from '@prismicio/client'
 import { SheetClose } from '../../ui/sheet'
 
-const client = createClient()
 
-const NavServices = async () => {
+
+const NavServices =  () => {
  
-   const services = await client.getSingle('navigation')
-  // const [services, setServices] = React.useState<GroupField<
-  //   Simplify<NavigationDocumentDataServicesItem>
-  // > | null>(null);
 
-  // React.useEffect(() => {
-  //   const fetchData = async () => {
-  //     const client = createClient(); 
-     
-  //       const {
-  //         data: { services },
-  //       } = await client.getSingle("navigation");
-  //       setServices(services);
-     
-  //   };
+  const [services, setServices] = React.useState<GroupField<
+    Simplify<NavigationDocumentDataServicesItem>
+  > | null>(null);
 
-  //   fetchData();
-  // }, []);
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const client = createClient(); 
+     
+        const {
+          data: { services },
+        } = await client.getSingle("navigation");
+        setServices(services);
+     
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <Accordion type="single" collapsible className="w-full">
       <AccordionItem value="item-1">
         <AccordionTrigger>Services</AccordionTrigger>
         <AccordionContent className='flex flex-col '>
-        {services?.data?.services.map((component) => (
+        {services?.map((component) => (
                 
                 <PrismicNextLink key={component.name} field={component.link}>
                   <SheetClose className='py-2'>
--- components/shared/Nav/navbar.tsx
+++ components/shared/Nav/navbar.tsx
@@ -79,6 +79,7 @@ export function Navbar() {
               <ul className="grid w-[300px] gap-3 p-4 md:w-[400px] md:grid-cols-2 lg:w-[500px] ">
                 {services?.map(({ name, summary, link }, idx) => (
                   <li key={idx}>
+                    <NavigationMenuLink asChild>
                     <PrismicNextLink field={link}>
                       <div className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
                         <div className="text-sm font-medium leading-none">
@@ -90,6 +91,8 @@ export function Navbar() {
                         </p>
                       </div>
                     </PrismicNextLink>
+                    </NavigationMenuLink>
+                  
                   </li>
                 ))}
 
